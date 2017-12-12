9e13b626088baf08c1dc2d32e247ddac050bd4b5
jabber <2898117012@qq.com>
2017-12-12 10:22:35 +0800

    refactor(money): update money class

--- src/Money.php
+++ src/Money.php
@@ -7,23 +7,14 @@ class Money {
 	private $uppers = [ '零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖' ];
 	private $units = [ '分', '角' ];
 	private $grees = [ '元', '拾', '佰', '仟', '万', '拾', '佰', '仟', '亿', '拾', '佰', '仟', '万', '拾', '佰' ];
-	private $isInt = false;
-	private $_intPartArray = [];
-	private $isFloat = false;
-	private $_floatPartArray = [];
-	private $likeInt = false;
+	private $thanOne = false;
 
 	public function __construct( $money ) {
 		if ( ! ( is_float( $money ) || is_numeric( $money ) || is_int( $money ) ) ) {
 			throw new \InvalidArgumentException( $money );
 		}
-		if ( is_int( $money ) ) {
-			$this->isInt = true;
-		} else {
-			$this->isFloat = false;
-		}
 		if ( $money > 1 ) {
-			$this->likeInt = true;
+			$this->thanOne = true;
 		}
 		$this->money = number_format( $money, 2, '.', '' );
 	}
@@ -34,52 +25,59 @@ class Money {
 	 */
 	public function toCapital(): string {
 		@list( $intPart, $decimalPart ) = explode( '.', $this->money, 2 );
+		if ( $this->money == 0 ) {
+			return '零元';
+		}
 		$result = $this->getIntPart( $intPart );
 		$result .= $this->getDecimalPart( $decimalPart );
-
 
 		return $result;
 	}
 
 	public function getIntPart( $intPart ) {
-		$gree = strlen( $intPart ) - 1;
+		$result = '';
+		$gree   = strlen( $intPart ) - 1;
 		if ( $intPart > 0 ) {
 			for ( $i = 0; $i < strlen( $intPart ); $i ++ ) {
-				$num                   = $intPart[ $i ];
-				$this->_intPartArray[] = $this->uppers[ $num ] . $this->grees[ $gree -- ];
+				$num    = $intPart[ $i ];
+				$result .= $this->uppers[ $num ] . $this->grees[ $gree -- ];
 			}
-		} else {
-
 		}
+
+		$result = str_replace( '零亿', '亿零', $result );
+		$result = str_replace( '零万', '万零', $result );
+
+		$result = str_replace( '零拾', '零', $result );
+		$result = str_replace( '零佰', '零', $result );
+		$result = str_replace( '零仟', '零', $result );
+
+		$result = str_replace( '零零', '零', $result );
+		$result = str_replace( '零零', '零', $result );
+
+		$result = str_replace( '零亿', '亿', $result );
+		$result = str_replace( '零万', '万', $result );
+		$result = str_replace( '零元', '元', $result );
+
+		return $result;
 	}
 
 	public function getDecimalPart( $decimalPart ) {
+		$result = '';
 		if ( $decimalPart > 0 ) {
 			//处理小数部分
 			$unit = strlen( $decimalPart ) - 1;
 			for ( $i = 0; $i < strlen( $decimalPart ); $i ++ ) {
-				$num                     = $decimalPart[ $i ];
-				$this->_floatPartArray[] = $this->uppers[ $num ] . $this->units[ $unit -- ];
+				$num    = $decimalPart[ $i ];
+				$result .= $this->uppers[ $num ] . $this->units[ $unit -- ];
 			}
 		}
-
-		if ( $this->likeInt ) {
-			foreach ( $this->_floatPartArray as $key => $value ) {
-				if ( $value == '零角' ) {
-					$this->_floatPartArray[ $key ] = '零';
-				}
-				if ( $value == '零分' ) {
-					unset( $this->_floatPartArray[ $key ] );
-				}
-			}
+		$result = str_replace( '零分', '', $result );
+		if ( $this->thanOne ) {
+			$result = str_replace( '零角', '零', $result );
 		} else {
-			foreach ( $this->_floatPartArray as $key => $value ) {
-				if ( $value == '零角' || $value == '零分' ) {
-					unset( $this->_floatPartArray[ $key ] );
-				}
-			}
+			$result = str_replace( '零角', '', $result );
 		}
 
-		return implode( '', $this->_floatPartArray );
+		return $result;
 	}
 }
