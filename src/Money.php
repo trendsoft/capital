c56f579815cc10bf104ade05ee9f0f457f78d176
jabber <2898117012@qq.com>
2017-12-15 00:22:01 +0800

    test: add test method

--- src/Money.php
+++ src/Money.php
@@ -13,23 +13,46 @@ class Money
 {
     private $money;
 
-    private $uppers = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
+    private $uppers = [ '零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖' ];
 
-    private $units = ['分', '角'];
+    private $units = [ '分', '角' ];
 
-    private $grees = ['元', '拾', '佰', '仟', '万', '拾', '佰', '仟', '亿', '拾', '佰', '仟', '万', '拾', '佰'];
+    private $grees = [ '元', '拾', '佰', '仟', '万', '拾', '佰', '仟', '亿', '拾', '佰', '仟', '万', '拾', '佰' ];
 
     private $thanOne = false;
 
-    public function __construct($money)
+    /**
+     * Money constructor.
+     *
+     * @param int|number|string $money default 0
+     */
+    public function __construct( $money = 0 )
     {
-        if (!(is_float($money) || is_numeric($money) || is_int($money))) {
-            throw new \InvalidArgumentException($money);
+        $this->setMoney( $money );
+    }
+
+    /**
+     * Get Init Money
+     *
+     * @return string
+     */
+    public function getMoney(): string
+    {
+        return $this->money;
+    }
+
+    /**
+     * @param int|number|string $money default 0
+     */
+    public function setMoney( $money = 0 )
+    {
+        if ( ! ( is_float( $money ) || is_numeric( $money ) || is_int( $money ) ) ) {
+            throw new \InvalidArgumentException( $money );
         }
-        if ($money > 1) {
+        if ( $money > 1 ) {
             $this->thanOne = true;
         }
-        $this->money = number_format($money, 2, '.', '');
+        $this->money = number_format( $money, 2, '.', '' );
     }
 
     /**
@@ -39,60 +62,83 @@ class Money
      */
     public function toCapital(): string
     {
-        @list($intPart, $decimalPart) = explode('.', $this->money, 2);
-        if (0.0 === floatval($this->money)) {
+        @list( $intPart, $decimalPart ) = explode( '.', $this->money, 2 );
+        if ( 0.0 === floatval( $this->money ) ) {
             return '零元';
         }
-        $result = $this->getIntPart($intPart);
-        $result .= $this->getDecimalPart($decimalPart);
+        $result = $this->getIntPart( $intPart );
+        $result .= $this->getDecimalPart( $decimalPart );
 
         return $result;
     }
 
-    private function getIntPart($intPart)
+    /**
+     * Parse to Capital
+     *
+     * @param int|number|string $money default 0
+     *
+     * @return string
+     */
+    public function parse( $money ): string
+    {
+        $this->setMoney( $money );
+
+        return $this->toCapital();
+    }
+
+    /**
+     * Get Int Part
+     *
+     * @return string
+     */
+    private function getIntPart( $intPart )
     {
         $result = '';
-        $gree = strlen($intPart) - 1;
-        if ($intPart > 0) {
-            for ($i = 0; $i < strlen($intPart); ++$i) {
-                $num = $intPart[$i];
-                $result .= $this->uppers[$num].$this->grees[$gree--];
+        $gree   = strlen( $intPart ) - 1;
+        if ( $intPart > 0 ) {
+            for ( $i = 0; $i < strlen( $intPart ); ++ $i ) {
+                $num    = $intPart[ $i ];
+                $result .= $this->uppers[ $num ] . $this->grees[ $gree -- ];
             }
         }
 
-        $result = str_replace('零亿', '亿零', $result);
-        $result = str_replace('零万', '万零', $result);
+        $result = str_replace( '零亿', '亿零', $result );
+        $result = str_replace( '零万', '万零', $result );
 
-        $result = str_replace('零拾', '零', $result);
-        $result = str_replace('零佰', '零', $result);
-        $result = str_replace('零仟', '零', $result);
+        $result = str_replace( '零拾', '零', $result );
+        $result = str_replace( '零佰', '零', $result );
+        $result = str_replace( '零仟', '零', $result );
 
-        $result = str_replace('零零', '零', $result);
-        $result = str_replace('零零', '零', $result);
+        $result = str_replace( '零零', '零', $result );
+        $result = str_replace( '零零', '零', $result );
 
-        $result = str_replace('零亿', '亿', $result);
-        $result = str_replace('零万', '万', $result);
-        $result = str_replace('零元', '元', $result);
+        $result = str_replace( '零亿', '亿', $result );
+        $result = str_replace( '零万', '万', $result );
+        $result = str_replace( '零元', '元', $result );
 
         return $result;
     }
 
-    private function getDecimalPart($decimalPart)
+    /**
+     * Get Decimal Part
+     *
+     * @return string
+     */
+    private function getDecimalPart( $decimalPart )
     {
         $result = '';
-        if ($decimalPart > 0) {
-            //处理小数部分
-            $unit = strlen($decimalPart) - 1;
-            for ($i = 0; $i < strlen($decimalPart); ++$i) {
-                $num = $decimalPart[$i];
-                $result .= $this->uppers[$num].$this->units[$unit--];
+        if ( $decimalPart > 0 ) {
+            $unit = strlen( $decimalPart ) - 1;
+            for ( $i = 0; $i < strlen( $decimalPart ); ++ $i ) {
+                $num    = $decimalPart[ $i ];
+                $result .= $this->uppers[ $num ] . $this->units[ $unit -- ];
             }
         }
-        $result = str_replace('零分', '', $result);
-        if ($this->thanOne) {
-            $result = str_replace('零角', '零', $result);
+        $result = str_replace( '零分', '', $result );
+        if ( $this->thanOne ) {
+            $result = str_replace( '零角', '零', $result );
         } else {
-            $result = str_replace('零角', '', $result);
+            $result = str_replace( '零角', '', $result );
         }
 
         return $result;
--- tests/Unit/MoneyTest.php
+++ tests/Unit/MoneyTest.php
@@ -19,30 +19,61 @@ class MoneyTest extends TestCase
      */
     public function testStringMoney()
     {
-        new Money('bs');
+        new Money( 'bs' );
     }
 
     public function testFloatMoney()
     {
-        $money = new Money(999.02);
-        $this->assertInstanceOf(Money::class, $money);
+        $money = new Money( 999.02 );
+        $this->assertInstanceOf( Money::class, $money );
     }
 
     public function testFloatMoney2()
     {
-        $money = new Money('999.02');
-        $this->assertInstanceOf(Money::class, $money);
+        $money = new Money( '999.02' );
+        $this->assertInstanceOf( Money::class, $money );
     }
 
     public function testIntMoney()
     {
-        $money = new Money(999);
-        $this->assertInstanceOf(Money::class, $money);
+        $money = new Money( 999 );
+        $this->assertInstanceOf( Money::class, $money );
     }
 
     public function testIntMoney2()
     {
-        $money = new Money('999');
-        $this->assertInstanceOf(Money::class, $money);
+        $money = new Money( '999' );
+        $this->assertInstanceOf( Money::class, $money );
+    }
+
+    public function testNullMoney()
+    {
+        $money = new Money();
+        $this->assertInstanceOf( Money::class, $money );
+    }
+
+    public function testSetMoney()
+    {
+        $money = new Money();
+        $money->setMoney( 0 );
+        $this->assertEquals( 0, $money->getMoney() );
+    }
+
+    public function testGetMoney()
+    {
+        $money = new Money();
+        $this->assertEquals( 0, $money->getMoney() );
+    }
+
+    public function testToCapital()
+    {
+        $money = new Money();
+        $this->assertEquals( '零元', $money->toCapital() );
+    }
+
+    public function testParse()
+    {
+        $money = new Money();
+        $this->assertEquals( '壹元', $money->parse( 1 ) );
     }
 }
